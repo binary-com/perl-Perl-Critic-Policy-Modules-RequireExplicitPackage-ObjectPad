d0b8c5ab4c45c86a6afd7eec2403ba2756ad9ee4
chylli-binary <chylli@binary.com>
2022-08-04 14:36:55 +0800

    init

---
+++ lib/Perl/Critic/Policy/Modules/RequireExplicitPackage/ObjectPad.pm
@@ -0,0 +1,36 @@
+package Perl::Critic::Policy::Modules::RequireExplicitPackage::ObjectPad;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Perl::Critic::Policy::Modules::RequireExplicitPackage::ObjectPad - Module abstract
+
+=head1 SYNOPSIS
+
+    use Perl::Critic::Policy::Modules::RequireExplicitPackage::ObjectPad;
+    my $instance = Perl::Critic::Policy::Modules::RequireExplicitPackage::ObjectPad->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
